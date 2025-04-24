50cbcf6f52bbeb89f23782e9b2855a32ae5a1565
Harsaamreta <skaadrn@gmail.com>
2025-04-24 14:52:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Varick ganteng ")
+st.title("🎈 Varick ganteng 🤩")
 st.write(
-    " aku gatau kenapa aku ganteng banget 😍 "
+    "aku gatau kenapa aku ganteng banget 😍"
 )
